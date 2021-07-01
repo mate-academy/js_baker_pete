83e14c5c822f0246a1c2b91985b69db85ac88c10
anna <anna.sydorenko.m@gmail.com>
2021-07-01 14:45:15 +0300

    Addition task

--- src/countCakes.test.js
+++ src/countCakes.test.js
@@ -15,7 +15,7 @@ describe(`Function 'countCakes':`, () => {
 
   it(`should return the number of cakes 
     when Pete has as many ingredients as the recipe requires`, () => {
-    const recepi = {
+    const recipe = {
       flour: 500,
       sugar: 200,
       eggs: 5,
@@ -26,12 +26,12 @@ describe(`Function 'countCakes':`, () => {
       eggs: 5,
     };
 
-    expect(countCakes(recepi, available)).toBe(1);
+    expect(countCakes(recipe, available)).toBe(1);
   });
 
   it(`should returns the maximum number of cakes
   that Pete can bake`, () => {
-    const recepi = {
+    const recipe = {
       flour: 500,
       sugar: 200,
       eggs: 5,
@@ -42,12 +42,12 @@ describe(`Function 'countCakes':`, () => {
       eggs: 25,
     };
 
-    expect(countCakes(recepi, available)).toBe(5);
+    expect(countCakes(recipe, available)).toBe(5);
   });
 
-  it(`should returns '0' when one of the ingredient
+  it(`should returns '0' when one of the ingredients
    is in the 'recipe' but there is no in the 'available'`, () => {
-    const recepi = {
+    const recipe = {
       flour: 500,
       sugar: 200,
       eggs: 5,
@@ -57,12 +57,12 @@ describe(`Function 'countCakes':`, () => {
       sugar: 100,
     };
 
-    expect(countCakes(recepi, available)).toBe(0);
+    expect(countCakes(recipe, available)).toBe(0);
   });
 
   it(`should return the maximum number of cakes
-  when 'available' ingredient has more value then in 'recipe'`, () => {
-    const recepi = {
+  when 'available' ingredient have more value then in 'recipe'`, () => {
+    const recipe = {
       flour: 500,
       sugar: 200,
       eggs: 5,
@@ -75,11 +75,11 @@ describe(`Function 'countCakes':`, () => {
       apples: 5,
     };
 
-    expect(countCakes(recepi, available)).toBe(2);
+    expect(countCakes(recipe, available)).toBe(2);
   });
 
-  it(`should return the round maximum number of cakes`, () => {
-    const recepi = {
+  it(`should return the round maximum number of cakes '3'`, () => {
+    const recipe = {
       flour: 500,
       sugar: 200,
       eggs: 5,
@@ -94,13 +94,13 @@ describe(`Function 'countCakes':`, () => {
       apples: 15,
     };
 
-    expect(countCakes(recepi, available)).toBe(3);
+    expect(countCakes(recipe, available)).toBe(3);
   });
 
   it(`should return '0' 
   when 'available' ingredient values less than
   'recipe' ingredient values`, () => {
-    const recepi = {
+    const recipe = {
       flour: 500,
       sugar: 200,
       eggs: 5,
@@ -111,6 +111,6 @@ describe(`Function 'countCakes':`, () => {
       eggs: 3,
     };
 
-    expect(countCakes(recepi, available)).toBe(0);
+    expect(countCakes(recipe, available)).toBe(0);
   });
 });
