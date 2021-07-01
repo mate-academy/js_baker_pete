3e6257a29b671ec005efef9bdb6cb6b40a9349be
Diana Kyrychuk <dianakyrychuk343@gmail.com>
2021-07-01 15:34:59 +0300

    solution

--- src/countCakes.test.js
+++ src/countCakes.test.js
@@ -8,13 +8,42 @@ describe(`Function 'countCakes':`, () => {
   });
 
   it(`should return a number`, () => {
-
+    expect(typeof countCakes()).toBe('number');
   });
 
-  it(`should return the number of cakes 
-    when Pete has as many ingredients as the recipe requires`, () => {
-
+  it(`should return 0 if objects are empty`, () => {
+    expect(countCakes({}, {})).toBe(0);
   });
 
-  // write your tests here
+  it(`should return the number of cakes due to ingradients`, () => {
+    const recipe = {
+      flour: 500,
+      sugar: 200,
+      eggs: 1,
+    };
+    const available = {
+      flour: 1000,
+      sugar: 400,
+      eggs: 5,
+      milk: 200,
+    };
+
+    expect(countCakes(recipe, available)).toBe(2);
+  });
+
+  it(`should consider ingredients are not present in the objects as 0`, () => {
+    const recipe = {
+      apples: 3,
+      sugar: 150,
+      flour: 300,
+      milk: 100,
+      oil: 100,
+    };
+    const available = {
+      flour: 50,
+      sugar: 100,
+    };
+
+    expect(countCakes(recipe, available)).toBe(0);
+  });
 });
