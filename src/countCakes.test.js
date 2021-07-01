ea85a142811d245c6f2ece189468846ca910704d
Kateryna Tymchenko <kate.timchenko.2001@gmail.com>
2021-07-01 21:10:16 +0300

    Solution with added chages

--- src/countCakes.test.js
+++ src/countCakes.test.js
@@ -13,49 +13,86 @@ describe(`Function 'countCakes':`, () => {
 
   it(`should return the maximum number of cakes that
   Pete can bake according to the recipe`, () => {
-    expect(countCakes({
-      flour: 500, sugar: 200, eggs: 1,
-    },
-    {
-      flour: 1200, sugar: 1200, eggs: 5, milk: 200,
-    })).toBe(2);
+    const rcp = {
+      flour: 500,
+      sugar: 200,
+      eggs: 1,
+    };
+
+    const ing = {
+      flour: 1200,
+      sugar: 1200,
+      eggs: 5,
+      milk: 200,
+    };
+
+    expect(countCakes(rcp, ing)).toBe(2);
   });
 
   it(`should return 1 when Pete has the same number
   of ingredients as the recipe requires`, () => {
-    expect(countCakes({
-      flour: 500, sugar: 200, eggs: 1,
-    },
-    {
-      flour: 500, sugar: 200, eggs: 1,
-    })).toBe(1);
+    const rcp = {
+      flour: 500,
+      sugar: 200,
+      eggs: 1,
+    };
+
+    const ing = {
+      flour: 500,
+      sugar: 200,
+      eggs: 1,
+    };
+
+    expect(countCakes(rcp, ing)).toBe(1);
   });
 
   it(`should return 0 when Pete has
   not enough ingredients as the recipe requires`, () => {
-    expect(countCakes({
-      apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100,
-    },
-    {
-      sugar: 500, flour: 2000, milk: 2000,
-    })).toBe(0);
+    const rcp = {
+      apples: 3,
+      flour: 300,
+      sugar: 150,
+      milk: 100,
+      oil: 100,
+    };
+
+    const ing = {
+      sugar: 500,
+      flour: 2000,
+      milk: 2000,
+    };
+
+    expect(countCakes(rcp, ing)).toBe(0);
   });
 
   it(`should consider the ingredients that are not present as 0`, () => {
-    expect(countCakes({
-      apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100,
-    },
-    {
-      sugar: 500, flour: 2000, milk: 2000,
-    })).toBe(0);
+    const rcp = {
+      flour: 300,
+      sugar: 150,
+      milk: 100,
+      oil: 100,
+    };
+    const ing = {
+      sugar: 500,
+      flour: 2000,
+      milk: 2000,
+    };
+
+    expect(countCakes(rcp, ing)).toBe(0);
   });
 
   it(`should return the type 'number' for ingredient values`, () => {
     const rcp = {
-      flour: 300, sugar: 150, milk: 100, oil: 100,
+      flour: 300,
+      sugar: 150,
+      milk: 100,
+      oil: 100,
     };
     const ing = {
-      apples: 3, sugar: 500, flour: 2000, milk: 2000,
+      apples: 3,
+      sugar: 500,
+      flour: 2000,
+      milk: 2000,
     };
 
     for (const i in rcp) {
@@ -63,9 +100,5 @@ describe(`Function 'countCakes':`, () => {
         expect(typeof countCakes(rcp[i], ing[j])).toBe('number');
       }
     }
-  });
-
-  it(`should return 0 if there are no intups`, () => {
-    expect(countCakes({}, {})).toBe(0);
   });
 });
