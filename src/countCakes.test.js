e50b67db454bbe10a925bc84979a14b499414305
Alkayde <morozovnikita5899@gmail.com>
2021-07-01 17:22:37 +0300

    Test after review

--- src/countCakes.test.js
+++ src/countCakes.test.js
@@ -14,36 +14,67 @@ describe(`Function 'countCakes':`, () => {
   it(`should return 1 
     when Pete has as many ingredients as the recipe requires`, () => {
     expect(countCakes({
-      flour: 100, sugar: 100, darkChocolate: 100, sourCream: 100, eggs: 2,
+      flour: 100,
+      sugar: 100,
+      darkChocolate: 100,
+      sourCream: 100,
+      eggs: 2,
     }, {
-      flour: 100, sugar: 100, darkChocolate: 100, sourCream: 100, eggs: 2,
+      flour: 100,
+      sugar: 100,
+      darkChocolate: 100,
+      sourCream: 100,
+      eggs: 2,
     })).toBe(1);
   });
 
   it(`should return 0 if 
     Pete has less available ingridients than recipe requires`, () => {
     expect(countCakes({
-      flour: 100, sugar: 100, darkChocolate: 100, sourCream: 100, eggs: 2,
+      flour: 100,
+      sugar: 100,
+      darkChocolate: 100,
+      sourCream: 100,
+      eggs: 2,
     }, {
-      flour: 50, sugar: 50, darkChocolate: 50, sourCream: 50, eggs: 1,
+      flour: 50,
+      sugar: 50,
+      darkChocolate: 50,
+      sourCream: 50,
+      eggs: 1,
     })).toBe(0);
   });
 
   it(`should return 0 if 
   Pete hasn't got any of ingridients that recipe requires`, () => {
     expect(countCakes({
-      flour: 100, sugar: 100, darkChocolate: 100, sourCream: 100, eggs: 2,
+      flour: 100,
+      sugar: 100,
+      darkChocolate: 100,
+      sourCream: 100,
+      eggs: 2,
     }, {
-      sugar: 100, darkChocolate: 100, sourCream: 100, eggs: 2,
+      sugar: 100,
+      darkChocolate: 100,
+      sourCream: 100,
+      eggs: 2,
     })).toBe(0);
   });
 
   it(`should return number of cakes if 
   Pete has more available ingridients than recipe requires`, () => {
     expect(countCakes({
-      flour: 100, sugar: 100, darkChocolate: 100, sourCream: 100, eggs: 2,
+      flour: 100,
+      sugar: 100,
+      darkChocolate: 100,
+      sourCream: 100,
+      eggs: 2,
     }, {
-      flour: 700, sugar: 600, darkChocolate: 900, sourCream: 500, eggs: 16,
+      flour: 700,
+      sugar: 600,
+      darkChocolate: 900,
+      sourCream: 500,
+      eggs: 16,
     })).toBe(5);
   });
 });
