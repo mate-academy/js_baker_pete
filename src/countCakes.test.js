c3bd2ad8accc64c550c98f3664a27198ae86ea8c
anna <anna.sydorenko.m@gmail.com>
2021-07-01 13:46:02 +0300

    Extra task done

--- src/countCakes.test.js
+++ src/countCakes.test.js
@@ -8,13 +8,109 @@ describe(`Function 'countCakes':`, () => {
   });
 
   it(`should return a number`, () => {
+    const res = countCakes();
 
+    expect(typeof res).toBe('number');
   });
 
   it(`should return the number of cakes 
     when Pete has as many ingredients as the recipe requires`, () => {
+    const recepi = {
+      flour: 500,
+      sugar: 200,
+      eggs: 5,
+    };
+    const available = {
+      flour: 500,
+      sugar: 200,
+      eggs: 5,
+    };
 
+    expect(countCakes(recepi, available)).toBe(1);
   });
 
-  // write your tests here
+  it(`should returns the maximum 
+  number of cakes that Pete can bake`, () => {
+    const recepi = {
+      flour: 500,
+      sugar: 200,
+      eggs: 5,
+    };
+    const available = {
+      flour: 2500,
+      sugar: 100,
+      eggs: 25,
+    };
+
+    expect(countCakes(recepi, available)).toBe(5);
+  });
+
+  it(`should returns '0' when one of the ingredient
+   is in the 'recipe' but there is no in the 'available'`, () => {
+    const recepi = {
+      flour: 500,
+      sugar: 200,
+      eggs: 5,
+    };
+    const available = {
+      flour: 2500,
+      sugar: 100,
+    };
+
+    expect(countCakes(recepi, available)).toBe(0);
+  });
+
+  it(`should return the maximum number of cakes 
+    when 'avalible' ingredient has more value then in 'recepi`, () => {
+    const recepi = {
+      flour: 500,
+      sugar: 200,
+      eggs: 5,
+    };
+    const available = {
+      flour: 1000,
+      sugar: 400,
+      eggs: 10,
+      milk: 200,
+      apples: 5,
+    };
+
+    expect(countCakes(recepi, available)).toBe(2);
+  });
+
+  it(`should return the round maximum number of cakes`, () => {
+    const recepi = {
+      flour: 500,
+      sugar: 200,
+      eggs: 5,
+      milk: 200,
+      apples: 5,
+    };
+    const available = {
+      flour: 1750,
+      sugar: 750,
+      eggs: 18,
+      milk: 700,
+      apples: 15,
+    };
+
+    expect(countCakes(recepi, available)).toBe(3);
+  });
+
+  it(`should return '0' 
+    when 'available' ingredient values less than 'recepi'
+    ingredient values`, () => {
+    const recepi = {
+      flour: 500,
+      sugar: 200,
+      eggs: 5,
+    };
+    const available = {
+      flour: 300,
+      sugar: 200,
+      eggs: 3,
+    };
+
+    expect(countCakes(recepi, available)).toBe(0);
+  });
 });
